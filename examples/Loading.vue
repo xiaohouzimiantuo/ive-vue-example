1433fbd46544efd8ee25308b9a19914471d41820
iverryluo <1292502340@qq..com>
2021-04-05 01:14:03 +0800

    更新文档

--- examples/Loading.vue
+++ examples/Loading.vue
@@ -9,23 +9,21 @@ import {
   onMounted,
 } from "vue";
 
-import { loading } from "ive-vue";
-// import { loading } from "@/components";
+import { IveLoading } from "ive-vue";
 export default defineComponent({
   setup() {
-
     onBeforeMount(() => {});
     onMounted(() => {
-      const loadingInstance = loading({text:'加载中'});
+      const loadingInstance = IveLoading.show({text:'加载中'});
       setTimeout(() => {
         // 关闭当前实例
         loadingInstance.close();
       }, 2000);
       setTimeout(() => {
-        loading('再次加载');
+        IveLoading.show('再次加载');
         setTimeout(() => {
           // 清除所有
-          loading.clear();
+          IveLoading.clear();
         }, 2000);
       }, 4000);
     });
